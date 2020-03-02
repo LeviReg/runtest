87fef7cdef47bbf7db547f15152560eeadd54324
Levi Regan <S00108881@mail.itsligo.ie>
2020-03-02 18:51:41 +0000

    dont konw what this is

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -8,15 +8,23 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicStorageModule.forRoot()
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Geolocation
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,124 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Platform, AlertController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Storage } from '@ionic/storage';
+import { filter } from 'rxjs/operators';
+
+declare var google;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  @ViewChild('map', { static: false }) mapElement: ElementRef;
+  map: any;
+  currentMapTrack = null;
 
-  constructor() {}
+  isTracking = false;
+  trackedRoute = [];
+  previousTracks = [];
 
+  positionSubscription: Subscription;
+
+  constructor(
+    public nacCtrl: NavController,
+    private plt: Platform,
+    private geolocation: Geolocation,
+    private storage: Storage,
+    private alertController: AlertController
+  ) {}
+
+  ngAfterViewInit() {
+    this.plt.ready().then(() => {
+      this.loadHistoricRoutes();
+      this.loadMap();
+    });
+  }
+
+  loadMap() {
+    let mapOptions = {
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      streetViewControl: false,
+      fullscreenControl: false
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    this.geolocation.getCurrentPosition().then(pos => {
+      let latLng = new google.maps.LatLng(
+        pos.coords.latitude,
+        pos.coords.longitude
+      );
+      this.map.setCenter(latLng);
+      this.map.setZoom(15);
+    });
+  }
+
+  loadHistoricRoutes() {
+    this.storage.get('routes').then(data => {
+      if (data) {
+        this.previousTracks = data;
+      }
+    });
+  }
+
+  startTracking() {
+    this.isTracking = true;
+    this.trackedRoute = [];
+
+    this.positionSubscription = this.geolocation
+      .watchPosition()
+      .pipe(
+        filter(p => p.coords !== undefined) //Filter Out Errors
+      )
+      .subscribe(data => {
+        setTimeout(() => {
+          this.trackedRoute.push({
+            lat: data.coords.latitude,
+            lng: data.coords.longitude
+          });
+          this.redrawPath(this.trackedRoute);
+        }, 0);
+      });
+  }
+
+  redrawPath(path) {
+    if (this.currentMapTrack) {
+      this.currentMapTrack.setMap(null);
+    }
+
+    if (path.length > 1) {
+      this.currentMapTrack = new google.maps.Polyline({
+        path: path,
+        geodesic: true,
+        strokeColor: '#ff00ff',
+        strokeOpacity: 1.0,
+        strokeWeight: 3
+      });
+      this.currentMapTrack.setMap(this.map);
+    }
+  }
+
+  stopTracking() {
+    let newRoute = { finished: new Date().getTime(), path: this.trackedRoute };
+    this.previousTracks.push(newRoute);
+    this.storage.set('routes', this.previousTracks);
+
+    this.isTracking = false;
+    this.positionSubscription.unsubscribe();
+    this.currentMapTrack.setMap(null);
+  }
+
+  showHistoryRoute(route) {
+    this.redrawPath(route);
+    console.log(route);
+  }
+
+  clearTracks(){
+    this.previousTracks.length = 0;
+  }
 }
